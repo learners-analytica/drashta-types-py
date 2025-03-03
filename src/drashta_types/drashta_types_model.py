16c31dd4edf15bbe381b27f1a108e0d0ce91970b
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-03 14:39:41 +0530

    update types

--- src/drashta_types/drashta_types_model.py
+++ src/drashta_types/drashta_types_model.py
@@ -2,12 +2,11 @@ from pydantic import BaseModel
 from .drashta_types_key import MLTaskTypes
 from .drashta_types_data import TDataSeriesMinimal
 
-TModelMetadata = {
-    "id": str,
-    "name": str,
-    "estimator": str,
-    "task": MLTaskTypes,
-    "date": str,
-    "columns": list[TDataSeriesMinimal],
-    "target":str
-}
+class TModelMetadata:
+    id: str
+    name: str
+    estimator: str
+    task: MLTaskTypes
+    date: str
+    columns: list[TDataSeriesMinimal]
+    target: str
